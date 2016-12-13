4b284ee22bc6ca2562dfa1d4c74d18ad0b88e771
Mihai Popescu <mihai@unity3d.com>
2016-12-14 00:35:45 +0100

    added scene with one random generated maze (w/ backtracking)

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MazeGeneration
+{
+    [Flags]
+    public enum CellState
+    {
+        Top     = 1 << 0,
+        Right   = 1 << 1,
+        Bottom  = 1 << 2,
+        Left    = 1 << 3,
+        Visited = 1 << 4,
+        Initial = Top | Left | Bottom | Right,
+    }
+
+    public struct Point
+    {
+        public int x, y;
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, System.Random rng)
+        {
+            var e = source.ToArray();
+            for (var i = e.Length - 1; i >= 0; i--)
+            {
+                var swapIndex = rng.Next(i + 1);
+                yield return e[swapIndex];
+                e[swapIndex] = e[i];
+            }
+        }
+
+        public static CellState OppositeWall(this CellState orig)
+        {
+            return (CellState)(((int) orig >> 2) | ((int) orig << 2)) & CellState.Initial;
+        }
+
+        public static bool HasFlag(this CellState cs, CellState flag)
+        {
+            return ((int)cs & (int)flag) != 0;
+        }
+
+        public static bool HasFlags(this CellState cs, CellState flag)
+        {
+            return ((int)cs & (int)flag) == (int)flag;
+        }
+    }
+
+    public class Maze
+    {
+        private readonly CellState[,] cells;
+        public readonly int width;
+        public readonly int height;
+        private readonly System.Random rng;
+
+        public Maze(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            cells = new CellState[width, height];
+            
+            for (var x = 0; x < width; x++)
+                for (var y = 0; y < height; y++)
+                    cells[x, y] = CellState.Initial;
+
+            rng = new System.Random();
+
+            VisitCell(rng.Next(width), rng.Next(height));
+        }
+        
+        public struct CellNeighbour
+        {
+            public int x, y;
+            public CellState side;
+        }
+
+        public IEnumerable<CellNeighbour> GetNeighbours(int x, int y)
+        {
+            if (x > 0)
+                yield return new CellNeighbour { x = x - 1, y = y,      side = CellState.Left }; 
+            if (y > 0)
+                yield return new CellNeighbour { x = x,     y = y - 1,  side = CellState.Top };
+            if (x < width - 1)
+                yield return new CellNeighbour { x = x + 1, y = y,      side = CellState.Right };
+            if (y < height - 1)
+                yield return new CellNeighbour { x = x,     y = y + 1,  side = CellState.Bottom };
+        }
+
+        private void VisitCell(int x, int y)
+        {
+            // start with a random cell and mark it as visited
+            this[x, y] |= CellState.Visited;
+            foreach(var n in GetNeighbours(x, y).Shuffle(rng).Where(z => !(this[z.x, z.y].HasFlag(CellState.Visited))))
+            {
+                cells[x, y]     &= ~n.side;
+                cells[n.x, n.y] &= ~n.side.OppositeWall();
+                VisitCell(n.x, n.y);
+            }
+        }
+
+        public CellState this[int x, int y]
+        {
+            get { return cells[x, y]; }
+            set { cells[x, y] = value; }
+        }
+    }
+}
---
+++ Assets/Scripts/MazeObject.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MazeGeneration;
+
+public class MazeObject : MonoBehaviour {
+    
+    Maze maze;
+
+    const int mazeWidth = 30;
+    const int mazeHeight = 20;
+
+    // Use this for initialization
+    void Start () {
+        maze = new Maze(mazeWidth, mazeHeight);
+
+        GetComponent<Renderer>().material.mainTexture = RenderMaze(maze);
+        InstantiateMaze(maze).transform.parent = transform;
+    }
+
+    static Texture2D RenderMaze(Maze maze)
+    {
+        Texture2D texture = new Texture2D(3 * maze.width, 3 * maze.height);
+
+        CellState[,] cs = new CellState[3, 3]
+        {
+                { CellState.Top | CellState.Left, CellState.Top, CellState.Top | CellState.Right },
+                { CellState.Left, CellState.Visited, CellState.Right },
+                { CellState.Bottom | CellState.Left, CellState.Bottom, CellState.Bottom | CellState.Right }
+        };
+
+        for (int y = 0; y < maze.height; y++)
+        {
+            for (int x = 0; x < maze.width; x++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        texture.SetPixel(x * 3 + j, y * 3 + i, maze[x, y].HasFlags(cs[i, j]) ? Color.white : Color.black);
+                    }
+                }
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    static GameObject InstantiateMaze(Maze maze)
+    {
+        Vector3 scale = new Vector3(10, 10, 10);
+        Vector3 move = new Vector3(-maze.width / 2.0f, 0, -maze.height / 2.0f);
+
+        GameObject anchor = new GameObject();
+
+        GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        floor.transform.SetParent(anchor.transform);
+        floor.transform.localScale = new Vector3(maze.width, 1, maze.height);
+
+        for (int y = 0; y < maze.height; y++)
+        {
+            for (int x = 0; x < maze.width; x++)
+            {
+                if (maze[x, y].HasFlags(CellState.Top))
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    go.transform.SetParent(anchor.transform);
+                    go.transform.position = Vector3.Scale(scale, move + new Vector3(x, 0.5f, y - 0.5f));
+                    go.transform.Rotate(-90, 0, 0);
+                }
+                if (maze[x, y].HasFlags(CellState.Left))
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    go.transform.SetParent(anchor.transform);
+                    go.transform.position = Vector3.Scale(scale, move + new Vector3(x - 0.5f, 0.5f, y));
+                    go.transform.Rotate(0, 0, -90);
+                }
+                if (maze[x, y].HasFlags(CellState.Right))
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    go.transform.SetParent(anchor.transform);
+                    go.transform.position = Vector3.Scale(scale, move + new Vector3(x + 0.5f, 0.5f, y));
+                    go.transform.Rotate(0, 0, 90);
+                }
+                if (maze[x, y].HasFlags(CellState.Bottom))
+                {
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    go.transform.SetParent(anchor.transform);
+                    go.transform.position = Vector3.Scale(scale, move + new Vector3(x, 0.5f, y + 0.5f));
+                    go.transform.Rotate(90, 0, 0);
+                }
+            }
+        }
+        return anchor;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+    	
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(10, 10, 100, 100),
+            GetComponent<Renderer>().material.mainTexture,
+            ScaleMode.ScaleToFit, true, 1.0f * mazeWidth/mazeHeight);
+    }
+}
